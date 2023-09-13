17c3449aff8502c4ab3e2a7cf82b4cd72d7934c9
Abdulmunim <amunim729@gmail.com>
2023-09-14 02:23:54 +0300

    Sorved CORS problem on localhost

--- engine/server.py
+++ engine/server.py
@@ -1,10 +1,12 @@
 from flask import Flask, jsonify, request
 from flask_restful import Resource, Api
+from flask_cors import CORS
 import chatbot
 
 print("Starting server...")
 
 app = Flask(__name__)
+CORS(app)
 api = Api(app)
 
 
