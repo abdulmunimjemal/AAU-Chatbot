f7631caaa7f3f787b883a3a697e806094e5b00c1
Abdulmunim <amunim729@gmail.com>
2023-09-14 02:25:31 +0300

    Added API integration and Sending Functions

--- web/js/main.js
+++ web/js/main.js
@@ -1,3 +1,6 @@
+const API = "http://localhost:5000/api/v1.0/";
+const errorMessage = "Sorry, I am not able to understand you. Please try again. I am still learning.";
+
 // JavaScript to automatically adjust the number of rows based on content
 const textarea = document.querySelector('.input');
 
@@ -6,16 +9,24 @@ textarea.addEventListener('input', function () {
     this.style.height = (this.scrollHeight) + 'px';
 });
 
-function appendMessage(msg, type) {
-    user = ['message-box-accent', 'message-box-right'];
-    bot = ['message-box-primary', 'message-box-left'];
-    classes = type === 'user' ? user : bot;
+function sendOnEnter(event) {
+    let key = event.key;
+    if (key === 'Enter') {
+        event.preventDefault();
+        document.querySelector('.send-btn').click();
+    } 
+}
 
+
+function appendMessage(msg, type) {
+    user = ['message-box--accent', 'message-box--right'];
+    bot = ['message-box--primary', 'message-box--left'];
+    classes = type === 'user' ? user : bot;
     // Create the box
     const message = document.createElement('div');
     message.classList.add('message-box');
-    message.classList.add('message-box--accent');
-    message.classList.add('message-box--right');
+    message.classList.add(classes[0]);
+    message.classList.add(classes[1]);
     // Create the content
     const content = document.createElement('p');
     content.classList.add('message-box__content');
@@ -34,6 +45,22 @@ form.addEventListener('click', function () {
     msg = msg.trim();
     if (msg.length > 0) {
         appendMessage(msg, 'user');
+        fetch(API + 'chatbot', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'access-control-allow-origin': '*' // CORS
+            },
+            body: JSON.stringify({ 'message': msg })
+        })
+            .then(response => response.json())
+            .then(data => {
+                appendMessage(data.message, 'bot');
+            })
+            .catch((error) => {
+                console.error('Error:', error);
+                appendMessage(errorMessage, 'bot');
+            })
     }
 });
 
