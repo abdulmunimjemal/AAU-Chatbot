e09727adcf24fe23feb628bee3196b2e26637e2d
Abdulmunim <amunim729@gmail.com>
2023-09-14 04:05:18 +0300

    Added typewriter effect

--- web/js/main.js
+++ web/js/main.js
@@ -1,71 +1,90 @@
 const API = "http://localhost:5000/api/v1.0/";
 const errorMessage = "Sorry, I am not able to understand you. Please try again. I am still learning.";
 
-// JavaScript to automatically adjust the number of rows based on content
 const textarea = document.querySelector('.input');
+const chatMessages = document.querySelector('.chatbot-container__messages');
 
+// Automatically adjust the number of rows based on content
 textarea.addEventListener('input', function () {
-    this.style.height = 'auto';
-    this.style.height = (this.scrollHeight) + 'px';
+  this.style.height = 'auto';
+  this.style.height = (this.scrollHeight) + 'px';
 });
 
 function sendOnEnter(event) {
-    let key = event.key;
-    if (key === 'Enter') {
-        event.preventDefault();
-        document.querySelector('.send-btn').click();
-    } 
+  if (event.key === 'Enter') {
+    event.preventDefault();
+    document.querySelector('.send-btn').click();
+  }
 }
 
-
-function appendMessage(msg, type) {
-    user = ['message-box--accent', 'message-box--right'];
-    bot = ['message-box--primary', 'message-box--left'];
-    classes = type === 'user' ? user : bot;
-    // Create the box
-    const message = document.createElement('div');
-    message.classList.add('message-box');
-    message.classList.add(classes[0]);
-    message.classList.add(classes[1]);
-    // Create the content
-    const content = document.createElement('p');
-    content.classList.add('message-box__content');
-    content.innerHTML = msg;
-    message.innerHTML = content.outerHTML;
-    // Append to chat
-    const chat = document.querySelector('.chatbot-container__messages');
-    chat.appendChild(message);
-    chat.scrollTop = chat.scrollHeight;
+function createMessageElement(msg, classes) {
+  const message = document.createElement('div');
+  message.classList.add('message-box', ...classes);
+  const content = document.createElement('p');
+  content.classList.add('message-box__content');
+  content.innerHTML = msg;
+  message.appendChild(content);
+  return message;
 }
+
+// Function to simulate typing effect for the p element's text content
+function typeMessage(targetElement, message, callback) {
+    const delay = 30; // Adjust the typing speed as needed (milliseconds)
+    let index = 0;
+    const typeInterval = setInterval(() => {
+      if (index < message.length) {
+        const char = message.charAt(index);
+        targetElement.textContent += char; // Append one character at a time
+        index++;
+      } else {
+        clearInterval(typeInterval);
+        // Call the callback function when typing is complete
+        if (typeof callback === 'function') {
+          callback();
+        }
+      }
+    }, delay);
+  }
+
+  function appendMessage(msg, type, typingEffect=true) {
+    const userClasses = ['message-box--accent', 'message-box--right'];
+    const botClasses = ['message-box--primary', 'message-box--left'];
+    const classes = type === 'user' ? userClasses : botClasses;
+    const message = createMessageElement(type==='bot' ? '' : msg, classes);
+    chatMessages.appendChild(message);
+    chatMessages.scrollTop = chatMessages.scrollHeight;
+  
+    if (typingEffect && type === 'bot') {
+      const pElement = message.querySelector('.message-box__content');
+      typeMessage(pElement, msg, () => {
+        // Typing is complete, you can add additional logic here if needed
+      });
+    }
+  }
 
 const form = document.querySelector('.send-btn');
-form.addEventListener('click', function () {
-    let msg = textarea.value;
-    textarea.value = '';
-    msg = msg.trim();
-    if (msg.length > 0) {
-        appendMessage(msg, 'user');
-        fetch(API + 'chatbot', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-                'access-control-allow-origin': '*' // CORS
-            },
-            body: JSON.stringify({ 'message': msg })
-        })
-            .then(response => response.json())
-            .then(data => {
-                appendMessage(data.message, 'bot');
-            })
-            .catch((error) => {
-                console.error('Error:', error);
-                appendMessage(errorMessage, 'bot');
-            })
-    }
+form.addEventListener('click', () => {
+  const msg = textarea.value.trim();
+  if (msg.length > 0) {
+    appendMessage(msg, 'user');
+    fetch(API + 'chatbot', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'access-control-allow-origin': '*' // CORS
+      },
+      body: JSON.stringify({ 'message': msg })
+    })
+      .then(response => response.json())
+      .then(data => {
+        appendMessage(data.message, 'bot');
+      })
+      .catch((error) => {
+        console.error('Error:', error);
+        appendMessage(errorMessage, 'bot');
+      })
+      .finally(() => {
+        textarea.value = '';
+      });
+  }
 });
-
-/* 
-<div class="message-box message-box--primary message-box--left">
-    <p class="message-box__content">Hey, I am a bot</p>  
-</div>
-*/
